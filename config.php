df73aa9a47d33f00f65881634653ff921bd32d92
rmac3392 <ryanjamesmacawili@gmail.com>
2023-11-04 09:38:12 +0800

    Wa nako kassabot

--- config.php
+++ config.php
@@ -22,28 +22,29 @@ define('HTTPS_SERVER', 'https://megalandrealty.com/');
 
 
 
-define('DIR_APPLICATION', '/home/u484898470/domains/megalandrealty.com/public_html/catalog/');
-define('DIR_SYSTEM', '/home/u484898470/domains/megalandrealty.com/public_html/system/');
-define('DIR_IMAGE', '/home/u484898470/domains/megalandrealty.com/public_html/image/');
-define('DIR_LANGUAGE', '/home/u484898470/domains/megalandrealty.com/public_html/catalog/language/');
-define('DIR_TEMPLATE', '/home/u484898470/domains/megalandrealty.com/public_html/catalog/view/theme/');
-define('DIR_CONFIG', '/home/u484898470/domains/megalandrealty.com/public_html/system/config/');
-define('DIR_CACHE', '/home/u484898470/domains/megalandrealty.com/public_html/system/storage/cache/');
-define('DIR_DOWNLOAD', '/home/u484898470/domains/megalandrealty.com/public_html/system/storage/download/');
-define('DIR_LOGS', '/home/u484898470/domains/megalandrealty.com/public_html/system/storage/logs/');
-define('DIR_MODIFICATION', '/home/u484898470/domains/megalandrealty.com/public_html/system/storage/modification/');
-define('DIR_UPLOAD', '/home/u484898470/domains/megalandrealty.com/public_html/system/storage/upload/');
+define('DIR_APPLICATION', '/var/www/html/Megaland/catalog/');
+define('DIR_SYSTEM', '/var/www/html/Megaland/system/');
+define('DIR_IMAGE', '/var/www/html/Megaland/image/');
+define('DIR_LANGUAGE', '/var/www/html/Megaland/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/Megaland/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/Megaland/system/config/');
+define('DIR_CACHE', '/var/www/html/Megaland/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/Megaland/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/Megaland/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/Megaland/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/Megaland/system/storage/upload/');
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 //define('DB_USERNAME', 'id19795493_admin');
 //define('DB_USERNAME', 'u484898470_9Rcth');
-define('DB_USERNAME', 'u484898470_test1');
 
-define('DB_PASSWORD', 'Karl123456789*');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '123');
+define('DB_DATABASE', 'u484898470_test1');
+
 //define('DB_DATABASE', 'id19795493_property');
 //define('DB_DATABASE', 'u484898470_Rs8Rp');
-define('DB_DATABASE', 'u484898470_test1');
 
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
